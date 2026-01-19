f0fd382458c72ae9125dde9b3707a236813f17c8
nikih94 <nikihuni@gmail.com>
2026-01-19 13:31:31 +0100

    added E2E delay

--- Dockerfile
+++ Dockerfile
@@ -1,20 +1,5 @@
-# FROM arm64v8/python:3.10-bookworm
-FROM python:3.10-bookworm
+FROM nikih94/audio_classification_base_image:latest
 
-# Set the working directory inside the container.
 WORKDIR /app
-
-# Copy the requirements file and install dependencies.
-COPY requirements.txt .
-RUN pip install --no-cache-dir -r requirements.txt
-
-# Copy code and metadata into /app.
-COPY src/ ./src/
-COPY metadata/ ./metadata/
-COPY resources/ ./resources/
-
-# Expose the port the Waitress server is listening on.
-EXPOSE 42002
-
-# Start the application
+COPY src ./src
 CMD ["python", "src/main.py"]
--- src/configuration.py
+++ src/configuration.py
@@ -3,11 +3,13 @@
 import configparser
 from dataclasses import dataclass
 
+
 @dataclass
 class ClassificationConfig:
     model_name: str
     window_size: int
     hop_size: int
+
 
 @dataclass
 class Influx2Config:
@@ -16,11 +18,14 @@ class Influx2Config:
     token: str
     log_bucket: str
     data_bucket: str
+    delay: bool
+
 
 @dataclass
 class AppConfig:
     classification: ClassificationConfig
     influx2: Influx2Config
+
 
 def load_config(filename: str) -> AppConfig:
     """Reads the INI file and returns an AppConfig object."""
@@ -28,17 +33,18 @@ def load_config(filename: str) -> AppConfig:
     parser.read(filename)
 
     classification = ClassificationConfig(
-        model_name=parser.get('classification', 'model_name'),
-        window_size=parser.getint('classification', 'window_size'),
-        hop_size=parser.getint('classification', 'hop_size'),
+        model_name=parser.get("classification", "model_name"),
+        window_size=parser.getint("classification", "window_size"),
+        hop_size=parser.getint("classification", "hop_size"),
     )
 
     influx2 = Influx2Config(
-        url=parser.get('influx2', 'url'),
-        org=parser.get('influx2', 'org'),
-        token=parser.get('influx2', 'token'),
-        log_bucket=parser.get('influx2', 'log_bucket'),
-        data_bucket=parser.get('influx2', 'data_bucket')
+        url=parser.get("influx2", "url"),
+        org=parser.get("influx2", "org"),
+        token=parser.get("influx2", "token"),
+        log_bucket=parser.get("influx2", "log_bucket"),
+        data_bucket=parser.get("influx2", "data_bucket"),
+        delay=parser.getboolean("influx2", "delay"),
     )
 
     return AppConfig(classification=classification, influx2=influx2)
